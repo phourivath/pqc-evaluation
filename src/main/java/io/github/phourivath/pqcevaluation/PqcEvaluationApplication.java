718b9725ea9211568d66fd2f755c47181b3b75be
SIN Phourivath <sinphourivath@gmail.com>
2026-08-17 09:15:27 +0700

    initial commit

---
+++ src/main/java/io/github/phourivath/pqcevaluation/PqcEvaluationApplication.java
@@ -0,0 +1,13 @@
+package io.github.phourivath.pqcevaluation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PqcEvaluationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PqcEvaluationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/phourivath/pqcevaluation/PqcEvaluationApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.phourivath.pqcevaluation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PqcEvaluationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
